eb97b4567b2a7626899c8a953d25be8df135942a
Alex Cuba <alex.cuba@stonybrook.edu>
2019-10-08 12:40:28 -0400

    Working List name and owner text fields

---
+++ src/components/list_screen/ListScreen.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+import ListHeading from './ListHeading'
+import ListItemsTable from './ListItemsTable'
+import ListTrash from './ListTrash'
+import PropTypes from 'prop-types';
+
+export class ListScreen extends Component {
+    state = {
+        name: this.getListName(),
+        owner: this.getListOwner()
+    }
+    getListName() {
+        if (this.props.todoList) {
+            let name = this.props.todoList.name;
+            return name;
+        }
+        else
+            return "";
+    }
+    getListOwner() {
+        if (this.props.todoList) {
+            let owner = this.props.todoList.owner;
+            return owner;
+        }
+    }
+    setListName = (e) => {
+        this.setState({name: e.target.value})
+        this.props.setListName(e.target.value);
+    }
+    setListOwner = (e) => {
+        this.setState({owner: e.target.value})
+        this.props.setListOwner(e.target.value);
+    }
+    render() {
+        return (
+            <div id="todo_list">
+                <ListHeading goHome={this.props.goHome} />
+                <ListTrash />
+                <div id="list_details_container">
+                    <div id="list_details_name_container" className="text_toolbar">
+                        <span id="list_name_prompt">Name:</span>
+                        <input 
+                            value={this.state.name}
+                            onChange = {this.setListName}
+                            type="text" 
+                            id="list_name_textfield" />
+                    </div>
+                    <div id="list_details_owner_container" className="text_toolbar">
+                        <span id="list_owner_prompt">Owner:</span>
+                        <input 
+                            value={this.state.owner}
+                            onChange = {this.setListOwner}
+                            type="text" 
+                            id="list_owner_textfield" />
+                    </div>
+                </div>
+                <ListItemsTable todoList={this.props.todoList} />
+            </div>
+        )
+    }
+}
+
+export default ListScreen
